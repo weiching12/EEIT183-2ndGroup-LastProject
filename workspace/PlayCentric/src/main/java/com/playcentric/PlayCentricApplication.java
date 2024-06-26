df5b8860c6249f3603b0feec4033f86836933b2d
TS8582 <a0958979918@gmail.com>
2024-06-26 10:18:51 +0800

    create project

---
+++ workspace/PlayCentric/src/main/java/com/playcentric/PlayCentricApplication.java
@@ -0,0 +1,13 @@
+package com.playcentric;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayCentricApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayCentricApplication.class, args);
+	}
+
+}
