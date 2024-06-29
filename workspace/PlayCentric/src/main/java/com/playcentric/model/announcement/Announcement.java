ff39d91e257d837b95dc3e608dc375e7f59ab528
TS8582 <a0958979918@gmail.com>
2024-06-29 13:24:17 +0800

    change anno

--- workspace/PlayCentric/src/main/java/com/playcentric/model/announcement/Announcement.java
+++ workspace/PlayCentric/src/main/java/com/playcentric/model/announcement/Announcement.java
@@ -1,19 +1,13 @@
 package com.playcentric.model.announcement;
 
 import java.io.Serializable;
-import java.sql.Date;
-import java.sql.Timestamp;
-import java.time.Instant;
-import java.time.LocalDateTime;
-import java.util.List;
+import java.util.Date;
 
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
-import org.hibernate.cache.spi.support.TimestampsRegionTemplate;
-import org.hibernate.type.descriptor.jdbc.TimestampJdbcType;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
@@ -21,13 +15,9 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.OneToOne;
-import jakarta.persistence.PrimaryKeyJoinColumn;
 import jakarta.persistence.Table;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
-import jakarta.persistence.Transient;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -44,7 +34,10 @@ public class Announcement implements Serializable{
 	private String content;
 	private Integer annoTypeId;
 	private Date createAt;
-	private Timestamp lastEditAt;
+	@JsonFormat(timezone = "GMT+8",pattern = "yyyy年MM月dd日 HH:mm:ss")
+	@DateTimeFormat(pattern = "yyyy年MM月dd日 HH:mm:ss")
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date lastEditAt;
 	@ManyToOne(fetch = FetchType.LAZY,cascade = CascadeType.ALL)
     @JoinColumn(name = "annoTypeId")
 	private AnnouncementType announcementType;
