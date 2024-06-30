debb7e7dce97731f2015b204ddc02e74a49244c2
QQisCuteDog <a0970598589@gmail.com>
2024-06-30 17:20:03 +0800

    06/30 道具倉庫entity OK

--- workspace/PlayCentric/src/main/java/com/playcentric/model/prop/MemberPropInventory.java
+++ workspace/PlayCentric/src/main/java/com/playcentric/model/prop/MemberPropInventory.java
@@ -1,5 +1,48 @@
 package com.playcentric.model.prop;
 
-public class MemberPropInventory {
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.MapsId;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "memberPropInventory")
+public class MemberPropInventory {
+	
+    @EmbeddedId
+    private MemberPropInventoryId id;
+
+//    FK props
+    @ManyToOne
+    @MapsId("propId")
+    @JoinColumn(name = "propId")
+    private Props props;
+	
+//    FK Member
+    @ManyToOne
+    @MapsId("menId")
+    @JoinColumn(name = "menId")
+//    private Member member;
+    
+    
+    private int propQuantity;
+
+	public MemberPropInventory(MemberPropInventoryId id, Props props, int propQuantity) {
+		super();
+		this.id = id;
+		this.props = props;
+		this.propQuantity = propQuantity;
+	}
+    
+    
+    
+    
 }
