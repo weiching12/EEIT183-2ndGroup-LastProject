c29f6fbb387b21fa90ed10b0f6e6864624acd1bb
TS8582 <a0958979918@gmail.com>
2024-07-01 08:43:37 +0800

    add game/gameType/image entity

---
+++ workspace/PlayCentric/src/main/java/com/playcentric/model/ImageLib.java
@@ -0,0 +1,33 @@
+package com.playcentric.model;
+
+import java.util.List;
+
+import com.playcentric.model.game.primary.Game;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "imageLib")
+public class ImageLib {
+	
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer imageId;
+	
+	private byte[] imageFile;
+	
+	@ManyToMany(mappedBy = "imageLibs")
+	private List<Game> games;
+}
