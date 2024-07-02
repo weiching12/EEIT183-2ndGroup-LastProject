a431251896d5cfcee7aeef65a5f3fbf208726b5a
OwenChen0414 <owen0414chen@gmail.com>
2024-07-02 11:36:55 +0800

    Entity 設定完畢

---
+++ workspace/PlayCentric/src/main/java/com/playcentric/model/member/GoogleLoginRepository.java
@@ -0,0 +1,7 @@
+package com.playcentric.model.member;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GoogleLoginRepository extends JpaRepository<GoogleLogin, Integer> {
+
+}
--- workspace/PlayCentric/src/main/java/com/playcentric/model/member/Member.java
+++ workspace/PlayCentric/src/main/java/com/playcentric/model/member/Member.java
@@ -1,10 +1,19 @@
 package com.playcentric.model.member;
 
+import java.util.Date;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -24,18 +33,42 @@ public class Member {
     private String email;
     private String nickname;
     private String memName;
-    private String birthday;
+
+	@JsonFormat(timezone = "GMT+8", pattern = "yyyy-MM-dd")
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date birthday;
     private String phone;
     private String address;
     private String googeId;
     private String facebookId;
     private String twitterId;
     private Integer totalSpent;
-    private String registDate;
-    private String lastLogin;
+
+	@JsonFormat(timezone = "GMT+8", pattern = "yyyy-MM-dd")
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date registDate;
+
+	@JsonFormat(timezone = "GMT+8", pattern = "yyyy-MM-dd HH:mm:ss.SSS")
+	@DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss.SSS")
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date lastLogin;
     private Short role;
     private Integer photo;
     private Short gender;
     private Short status;
     private Integer points;
+    
+    
+    @PrePersist // 當物件要轉換成 persistent 以前，先執行以下方法
+	public void onCreate() {
+		if (this.registDate == null) {
+			this.registDate = new Date();
+		}
+		if (this.registDate == null) {
+			this.registDate = new Date();
+		}
+	}
+    
 }
