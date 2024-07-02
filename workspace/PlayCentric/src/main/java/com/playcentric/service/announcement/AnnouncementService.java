52797b67922a252f7482cd86da9e0e1c94c932ec
TS8582 <a0958979918@gmail.com>
2024-07-02 15:54:50 +0800

    add announcement service

---
+++ workspace/PlayCentric/src/main/java/com/playcentric/service/announcement/AnnouncementService.java
@@ -0,0 +1,63 @@
+package com.playcentric.service.announcement;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.playcentric.model.announcement.Announcement;
+import com.playcentric.model.announcement.AnnouncementRepository;
+import com.playcentric.model.announcement.AnnouncementType;
+import com.playcentric.model.announcement.AnnouncementTypeRepository;
+
+@Service
+public class AnnouncementService {
+	
+	@Autowired
+	private AnnouncementRepository aRepo;
+	
+	@Autowired
+	private AnnouncementTypeRepository aTRepo;
+	
+	//用ID找公告
+	public Announcement findById(Integer id) {
+		return aRepo.findById(id).get();
+	}
+	
+	//取得全部公告
+	public List<Announcement> findAll() {
+		return aRepo.findAll();
+	}
+	
+	//更新公告
+	public Announcement update(Announcement announcement) {
+		Announcement anno = findById(announcement.getAnnoId());
+		anno.setAnnoTypeId(announcement.getAnnoTypeId());
+		anno.setAnnouncementType(announcement.getAnnouncementType());
+		anno.setContent(announcement.getContent());
+		anno.setLastEditAt(announcement.getLastEditAt());
+		anno.setTitle(announcement.getTitle());
+		return aRepo.save(anno);
+	}
+	
+	//新增公告
+	public Announcement insert(Announcement announcement) {
+		return aRepo.save(announcement);
+	}
+	
+	//刪除公告(id)
+	public void delete(Integer id) {
+		aRepo.deleteById(id);
+	}
+	
+	//id找公告類型
+	public AnnouncementType findTypeById(Integer id) {
+		return aTRepo.findById(id).get();
+	}
+	
+	//取得所有公告類型
+	public List<AnnouncementType> findAllType() {
+		return aTRepo.findAll();
+	}
+	
+}
