c3541178ba8e31bb85407445a44d10517c41d4cb
QQisCuteDog <a0970598589@gmail.com>
2024-07-03 18:10:19 +0800

    07/03搜尋全部道具

--- workspace/PlayCentric/src/main/java/com/playcentric/controller/prop/PropController.java
+++ workspace/PlayCentric/src/main/java/com/playcentric/controller/prop/PropController.java
@@ -1,43 +1,52 @@
-package com.playcentric.controller.prop;
-
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.SessionAttributes;
-
-import com.playcentric.model.game.primary.Game;
-import com.playcentric.service.game.GameService;
+//package com.playcentric.controller.prop;
+//
+//import java.util.List;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.ResponseBody;
+//import org.springframework.web.bind.annotation.SessionAttributes;
+//
+//import com.playcentric.model.game.primary.Game;
+//import com.playcentric.service.game.GameService;
 //import com.playcentric.service.prop.PropService;
-
-import ch.qos.logback.core.model.Model;
-
-@Controller
-@SessionAttributes(names = "games")
-public class PropController {
-
+//import com.playcentric.model.prop.Props;
+//
+//import ch.qos.logback.core.model.Model;
+//
+//@Controller
+//@SessionAttributes(names = "games")
+//public class PropController {
+//
 //	@Autowired
 //	private PropService propService;
-
-	@Autowired
-	private GameService gameService;
-	
-//	主頁
-	@GetMapping("/prop/propSheet")
-	public String addHouse() {
-		return "prop/propSheet";
-	}
-	
-	
-
-// 找尋所有遊戲回傳至選單
-	@GetMapping("/prop/SelectGame")
-	@ResponseBody
-	public List<Game> findAllGame() {
-		List<Game> games = gameService.findAll();
-        return games;
-
-	}
-}
+//
+//	@Autowired
+//	private GameService gameService;
+//	
+////	主頁
+//	@GetMapping("/prop/propSheet")
+//	public String addHouse() {
+//		return "prop/propSheet";
+//	}
+//	
+//	
+//
+//// 找尋所有遊戲回傳至選單
+//	@GetMapping("/prop/findAllGame")
+//	@ResponseBody
+//	public List<Game> findAllGame() {
+//		List<Game> games = gameService.findAll();
+//        return games;
+//
+//	}
+//	
+//// 根據遊戲ID找尋所有道具
+//	@GetMapping("/prop/findAllPropsByGameId")
+//	@ResponseBody
+//	public List<Props> =propService {
+//
+//
+//}
+//	}
